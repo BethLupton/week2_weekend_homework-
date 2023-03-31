a87baf2136fcd8c37c608b67857e6f9dfe766a69
Beth Lupton <bethlupton@martins-mbp.lan>
2023-03-31 17:03:03 +0100

    Third Commit

--- src/room.py
+++ src/room.py
@@ -4,3 +4,19 @@ class Room:
         self.songs = songs
         self.fee = fee
         self.capacity = capacity
+
+    def guest_count(self):
+        return len(self.guests)
+
+    def check_in_guest(self, guest):
+        self.guests.append(guest)
+
+    def check_out_guest(self, guest):
+        self.guests.remove.check_in_guest(guest)
+  
+
+    # def song_count(self):
+    #     return len(self.songs)
+
+    # def add_song(self, song):
+    #     self.songs.append(song)
--- tests/room_test.py
+++ tests/room_test.py
@@ -5,35 +5,43 @@ from src.song import Song
 
 
 class TestRoom(unittest.TestCase):
-        def setUp(self):
-            self.song1 = Song("The Walk", "The Cure")
-            self.song2 = Song("Train in Vain", "The Clash")
-            self.song3 = Song("This Must Be the Place", "Talking Heads")
-            self.song4 = Song("Honey Sweet", "Blossoms")
-            self.room1 = Room([], [self.song1], 20, 2)        
-            self.room2 = Room([], [self.song1], 50, 6)        
-            self.room3 = Room([], [self.song1], 110, 15)        
-            self.guest1 = Guest("Beth", 125, self.song1)
-            self.guest2 = Guest("Errin", 200, self.song4)
-            self.guest3 = Guest("Hannah", 75, self.song8)
+    def setUp(self):
+        self.song1 = Song("The Walk", "The Cure")
+        self.song2 = Song("Train in Vain", "The Clash")
+        self.song3 = Song("This Must Be the Place", "Talking Heads")
+        self.song4 = Song("Honey Sweet", "Blossoms")
+        self.guest1 = Guest("Beth", 125, self.song1)
+        self.guest2 = Guest("Errin", 200, self.song4)
+        self.guest3 = Guest("Hannah", 75, self.song2)
+        self.room1 = Room([], [], 20, 2)
+        self.room2 = Room([], [self.song1], 50, 6)
+        self.room3 = Room([self.guest3], [self.song1], 110, 15)
 
-            self.karaoke_bar = {
-                  "name": "Maggie's Mosh Pit",
-                  "rooms": [
-                  self.room1
-                  self.room2
-                  self.room3
-                  ]
-            }
+        self.karaoke_bar = {
+            "name": "Maggie's Mosh Pit",
+            "rooms": [
+                self.room1,
+                self.room2,
+                self.room3
+            ]
+        }
 
-    # def test_room_does_have_capacity(self):
-    # def test_room_does_not_have_capacity(self):
-    # def test_to_check_in_guest(self):
-    # def test_to_check_out_guest(self):
-    # def test_to_add_song(self):
-    # def test_to_remove_song(self):
-    # def test_to_find_guests_fave_song(self):
+    def test_to_check_in_guest(self):
+        self.room1.check_in_guest(self.guest1)
+        self.assertEqual(1, self.room1.guest_count())
 
 
+    def test_to_check_out_guest(self):
+        self.room1.check_out_guest(self.guest1)
+        self.assertEqual(1, self.room1.check_out_guest)
 
-   
+    # def test_to_add_song_to_room(self):
+    #     self.room1.add_song(self.song1)
+    #     self.assertEqual(1, self.room1.song_count())
+
+# def test_to_remove_song(self):
+
+
+# def test_to_find_guests_fave_song(self):
+# def test_room_does_have_capacity(self):
+# def test_room_does_not_have_capacity(self):
